bac423ab0c689a1cb793493bf4be87200cc075f0
alex462 <alex.reinhart462@gmail.com>
2019-01-02 01:57:25 -0500

    Shortened code for other projects' brevity

--- src/com/company/playGame.java
+++ src/com/company/playGame.java
@@ -25,90 +25,56 @@ public class playGame extends RunGame {
     //NUM GROUP 1
     //Includes smaller numbers 1, 2, 5, 6
     private void rnd2NumGp1() {
-        System.out.print("Now choose a number: \n2    5\n\n1     6\n");
-        int userRnd2NumGp1 = in.nextInt();
-        if (userRnd2NumGp1 == 1 || userRnd2NumGp1 == 5) {
-            origamiPaperNoise();
-            rnd3NumGp2();
-        }
-        if (userRnd2NumGp1 == 2 || userRnd2NumGp1 == 6) {
-            origamiPaperNoise();
-            rnd3NumGp1();
-        } else {
-            System.out.print("Invalid entry.");
-            System.exit(13);
+        System.out.print("Now choose a number: \n2      5\n\n1     6\n");
+        int userRnd3NumGp1 = in.nextInt();
+        switch (userRnd3NumGp1) {
+            case 1:
+                origamiPaperNoise();
+                System.out.print("run.");
+                break;
+            case 2:
+                origamiPaperNoise();
+                System.out.print("No snowflake in an avalanche ever feels responsible.");
+                break;
+            case 5:
+                origamiPaperNoise();
+                System.out.print("If you eat something, and nobody sees you eat it, it has no calories.");
+                break;
+            case 6:
+                origamiPaperNoise();
+                System.out.print("If you think no one cares if you are alive, try missing a couple car payments.");
+                break;
+            default:
+                System.out.print("");
+                break;
         }
     }
 
     //NUM GROUP 2
     //Includes larger numbers 3, 4, 7, 8
     private void rnd2NumGp2() {
-        System.out.print("Now choose a number: \n3    4\n\n8     7\n");
-        int userRnd2NumGp2 = in.nextInt();
-        if (userRnd2NumGp2 == 3 || userRnd2NumGp2 == 7) {
-            origamiPaperNoise();
-            rnd3NumGp1();
-        }
-        if (userRnd2NumGp2 == 4 || userRnd2NumGp2 == 8) {
-            origamiPaperNoise();
-            rnd3NumGp2();
-        } else {
-            System.out.print("Invalid entry.");
-            System.exit(13);
-        }
-    }
-
-    private void rnd3NumGp1() {
-        System.out.print("Now choose a number: \n2      5\n\n1     6\n");
-        int userRnd3NumGp1 = in.nextInt();
-        switch (userRnd3NumGp1) {
-            case 1:
-                origamiPaperNoise();
-                System.out.print("\brun.\b");
-                break;
-            case 2:
-                origamiPaperNoise();
-                System.out.print("\bNo snowflake in an avalanche ever feels responsible.\b");
-                break;
-            case 5:
-                origamiPaperNoise();
-                System.out.print("\bIf you eat something, and nobody sees you eat it, it has no calories.\b");
-                break;
-            case 6:
-                origamiPaperNoise();
-                System.out.print("\bIf you think no one cares if you are alive, try missing a couple car payments.\b");
-                break;
-            default:
-                System.out.print("Invalid entry.");
-                rnd3NumGp1();
-                break;
-        }
-    }
-
-    private void rnd3NumGp2() {
         System.out.print("Now choose a number: \n3      4\n\n8      7\n");
         int userRnd3NumGp2 = in.nextInt();
         switch (userRnd3NumGp2) {
             case 3:
                 origamiPaperNoise();
-                System.out.print("\bYour pet is planning to eat you.\b");
+                System.out.print("Your pet is planning to eat you.");
                 break;
             case 4:
                 origamiPaperNoise();
-                System.out.print("\bYour resemblance to a muppet will prevent the world from taking you seriously.\b");
+                System.out.print("Your resemblance to a muppet will prevent the world from taking you seriously.");
                 break;
             case 7:
                 origamiPaperNoise();
-                System.out.print("\bA true patriot is the fellow who gets a parking ticket and rejoices that the system works. " +
-                        "Are you a TRUE patriot?\b");
+                System.out.print("A true patriot is the fellow who gets a parking ticket and rejoices that the system works. " +
+                        "Are you a TRUE patriot?");
                 break;
             case 8:
                 origamiPaperNoise();
-                System.out.print("\bYour friends secretly agree that your head is the wrong size for your body.\b");
+                System.out.print("Your friends secretly agree that your head is the wrong size for your body.");
                 break;
             default:
-                System.out.print("Invalid entry.");
-                rnd3NumGp2();
+                System.out.print("");
                 break;
         }
     }
